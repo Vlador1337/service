ce6023c634f981c45b5a029d2562dc508d8e5d72
Vlador1337 <lolipop120999@gmail.com>
2022-05-14 23:23:44 +0500

    Database connection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
-public class Main {
+import java.sql.*;
+import java.nio.file.*;
+import java.io.*;
+import java.util.*;
+
+public class Main{
 
     public static void main(String[] args) {
-	// write your code here
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = getConnection()){
+
+                System.out.println("Connection to Store DB succesfull!");
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException, IOException{
+
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("database.properties"))){
+            props.load(in);
+        }
+        String url = props.getProperty("url");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+
+        return DriverManager.getConnection(url, username, password);
     }
 }
+
